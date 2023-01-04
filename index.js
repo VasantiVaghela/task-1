fba2d225907366125ed124c3103b578751d34a6e
Vasanti Kevit <vasantivaghela@kevit.io>
2023-01-04 10:45:57 +0530

    1st feature is modified

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var app = express();
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
+   const userCount = 10;
+   res.send(`Hello GET. Users are: ${userCount}`);
 })
 
 
